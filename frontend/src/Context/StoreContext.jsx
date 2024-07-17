68fcb11b9789b9c7cfcc81afd156da7636a041fb
Sameep Sadotra <sadotrasameep@gmail.com>
2024-07-17 12:55:49 +0530

    first

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -4,6 +4,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
+  const url = "http://localhost:4000"
+  const [token, setToken] = useState("")
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
@@ -26,6 +28,12 @@ const StoreContextProvider = (props) => {
     }
     return totalAmount;
   };
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+        setToken(localStorage.getItem("token"));
+    }
+}, []);
+
 
   const contextValue = {
     food_list,
@@ -33,7 +41,10 @@ const StoreContextProvider = (props) => {
     addToCart,
     removeFromCart,
     setCartItems,
-    getTotalCartAmount
+    getTotalCartAmount,
+    url,
+    token,
+    setToken
   };
   return (
     <StoreContext.Provider value={contextValue}>
