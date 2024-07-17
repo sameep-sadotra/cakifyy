c388e6b15480571d6a349d332d5b0cf18dfd3f61
Sameep Sadotra <sadotrasameep@gmail.com>
2024-07-17 19:56:42 +0530

    First

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import Home from "./pages/Home/Home";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
+import Verify from "./pages/Verify/Verify";
+import MyOrders from "./pages/MyOrders/MyOrders";
 
 
 const App =() => {
@@ -19,6 +21,8 @@ const App =() => {
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/verify" element={<Verify />}/>
+          <Route path="/myorders" element={<MyOrders/>}/>
         </Routes>
       </div>
       <Footer/>
